8dc88bc207b9f8f9e6c0a3ae3e2f908f03e3a2ca
Sotiris Ioannou <me@sotiris.engineer>
2016-09-07 20:57:04 -0400

    Added App,Header,Order,Inventory components

--- scripts/main.js
+++ scripts/main.js
@@ -4,13 +4,70 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 
+//App Component
+var App = React.createClass({
+   render : function() {
+       return(
+         <div className="catch-of-the-day">
+            <div className="menu">
+                <Header/>
+            </div>
+             <Order/>
+             <Inventory/>
+         </div>
+       )
+   } 
+});
+
+// Header
+var Header = React.createClass({
+
+    render : function(){
+
+        return(
+            <p>Header</p>
+        )
+
+
+    }
+});
+
+
+
+// Order
+var Order = React.createClass({
+
+    render : function(){
+
+        return(
+            <p>Order</p>
+        )
+
+
+    }
+});
+
+// Inventory
+var Inventory = React.createClass({
+
+    render : function(){
+
+        return(
+            <p>Inventory</p>
+        )
+
+
+    }
+});
+
+
+
 // StorePicker component
 //this will give us the <StorePicker/>
 var StorePicker = React.createClass({
    render : function () {
        //you can only return a single element
        return(
-       {/* these are comments in jsx */}
            <form className="store-selector">
                <h2>Please Enter a Store </h2>
                <input type="text" ref="storeId" required/>
@@ -22,4 +79,4 @@ var StorePicker = React.createClass({
 
 
 //DOM renderer
-ReactDOM.render(<StorePicker/>, document.querySelector('#main'));
+ReactDOM.render(<App/>, document.querySelector('#main'));
